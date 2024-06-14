d1c14ee430971f405b6101f9eea6bdfe43d710c3
dandaboy0 <ravinduanupama30@gmail.com>
2024-06-14 09:30:55 +0530

    up

--- index.php
+++ index.php
@@ -3,259 +3,198 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="x-UA-compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library Project</title>
     <link rel="stylesheet" href="assets/dist/css/bootstrap.min.css">
     <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Library Project</title>
     <!-- <style>
-        body {
-            background-image: url(OIP\ \(2\).jpeg);
-            background-repeat: no-repeat;
-            height: 100vh;
-            background-size: cover; 
-        }
-
-        body {
-            display: flex;
-            justify-content: flex-end;
-            align-items: normal;
-            height: 100vh;
+        * {
+            padding: 0;
             margin: 0;
-            background-color: #f0f0f0;
-            padding-right: 100px;
-            /* Optional: Adjust this value to add some padding from the right edge */
+            box-sizing: border-box;
+            font-family: sans-serif;
         }
 
-        .box {
-            width: 50%;
-            background: rgba(0, 0, 0, 0.5);
-            color: white;
-            border: 2px solid #333;
-            border-radius: 10px;
+        body {
             display: flex;
             justify-content: center;
             align-items: center;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.9);
+            height: 100vh;
+            background-image: linear-gradient(#380036, #0CBABA);
+        }
+
+        .containe {
+            max-width: 650px;
+            padding: 28px;
+            margin: 0 28px;
+            border-radius: 10px;
+            overflow: hidden;
+            background: rgba(0, 0, 0, 0.2);
+            box-shadow: 0 15px 20px rgba(0, 0, 0, 0.6);
+        }
+
+        .h2 {
+            font-size: 26px;
+            font-weight: bold;
+            text-align: left;
+            color: #fff;
+            padding-bottom: 8px;
+            border-bottom: 1px solid silver;
+        }
+
+        .content {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
             padding: 20px;
         }
     </style> -->
-
-
 </head>
 
 <body>
-    <form method="post">
-        <div class="box">
-
-            <div class="container">
-                <div class="row">
-                    <div class="col-6 mt-5">
-                        <p>
-                        <h5>MID</h5>
-                        </p>
-                    </div>
-                    <div class="col-6 mt-5">
-                        <p>
-                        <h5>CID</h5>
-                        </p>
-                    </div>
-
-                    <div class="col-6">
-                        <input type="text" class="form-control border-dark" placeholder="Enter Member ID" name="mid"
-                            id="mid" required>
-                    </div>
-                    <div class="col-6">
-                        <input type="text" class="form-control border-dark" placeholder="Enter Company ID" name="cid"
-                            id="cid" required>
-                    </div>
+    <div class="container">
+        <form action="" method="post">
+            <h2>Loging</h2>
+            <div class="content">
+                <div class="input-box">
+                    <label for="name">MID</label>
+                    <input type="number" placeholder="Enter Your Member ID" id="mid" required>
                 </div>
-
-                <div class="row mt-3">
-
-                    <div class="col-3">
-                        <p>
-                        <h5>Member Name</h5>
-                        </p>
-                    </div>
-
-                    <div class="col-9">
-                        <input type="text" class="form-control border-dark" placeholder="Enter Member Name" name="mname"
-                            id="mname" required>
-                    </div>
-
+            </div>
+            <div class="content">
+                <div class="input-box">
+                    <label for="name">CID</label>
+                    <input type="number" placeholder="Enter Your Company ID" id="cid" required>
                 </div>
-
-
-                <div class="row">
-                    <div class="col-3">
-                        <p>
-                        <h5>Address</h5>
-                        </p>
-                    </div>
-
-                    <div class="col-9">
-                        <input type="text" class="form-control border-dark" placeholder="Enter Address" name="madd"
-                            id="madd" required>
-                    </div>
-
+            </div>
+            <div class="content">
+                <div class="input-box">
+                    <label for="name">Member Name</label>
+                    <input type="text" placeholder="Enter Your Name" id="mname" required>
                 </div>
-
-
-                <div class="row">
-
-                    <div class="col-6">
-                        <p>
-                        <h5>Tellephon Num</h5>
-                        </p>
-                    </div>
-                    <div class="col-6">
-                        <p>
-                        <h5>Registar Date</h5>
-                        </p>
-                    </div>
-                    <div class="col-6">
-                        <input type="number" class="form-control border-dark" placeholder="Enter Tell Number"
-                            name="mtel" id="mtel" minlength=10>
-                    </div>
-                    <div class="col-6">
-                        <input type="date" class="form-control border-dark" name="mregdate" id="mregdate">
-                    </div>
-
+            </div>
+            <div class="content">
+                <div class="input-box">
+                    <label for="name">Address</label>
+                    <input type="text" placeholder="Enter Your Address" id="madd" required>
                 </div>
-
-                <div class="row">
-
-                    <div class="col-6">
-                        <p>
-                        <h5>NIC</h5>
-                        </p>
-                    </div>
-                    <div class="col-6">
-                        <p>
-                        <h5>Section</h5>
-                        </p>
-                    </div>
-
-                    <div class="col-6">
-                        <input type="number" class="form-control border-dark" placeholder="your NIC Num" name="mnic"
-                            id="mnic" required>
-                    </div>
-                    <div class="col-6">
-                        <div class="input-group ">
-                            <div class="input-group-prepend">
-                                <label class="input-group-text" for="msec">Options</label>
-                            </div>
-                            <select class="custom-select" id="msec">
-                                <option selected>Select Your Section :-</option>
-                                <option value="1">HR</option>
-                                <option value="2">Admin</option>
-                                <option value="3">Other</option>
-                            </select>
-                        </div>
-
-                    </div>
-
+            </div>
+            <div class="content">
+                <div class="input-box">
+                    <label for="name">Tellephon Num</label>
+                    <input type="tel" placeholder="Enter ure MIC" id="mtel" required>
                 </div>
-
-                <div class="row">
-
-                    <div class="col-6">
-                        <p>
-                        <h5>Birth Day</h5>
-                        </p>
-                    </div>
-                    <div class="col-6"></div>
-                    <div class="col-6">
-                        <input type="date" class="form-control border-dark" name="mbdate" id="mbdate" required>
-                    </div>
-
+            </div>
+            <div class="content">
+                <div class="input-box">
+                    <label for="name">Registar Date</label>
+                    <input type="date" placeholder="Enter ure MIC" id="mregdate" required>
                 </div>
-                <div class="row">
-                    <div class="col-4  mt-5">
-
-                    </div>
-                    <div class="col-4  mt-5">
-                        <input type="submit" class=" form-control btn btn-outline-success" onclick="find()" id="mcfirm">
-                    </div>
-                    <div class="col-4  mt-5">
-
-                    </div>
-
-
+            </div>
+            <div class="content">
+                <div class="input-box">
+                    <label for="name">NIC</label>
+                    <input type="tel" placeholder="Enter your National ID Number" id="mnic" required>
                 </div>
-
+            </div>
+            <div class="content">
+                <div class="input-box">
+                    <label for="name">Section</label>
+                    <select class="" name="age" required value="" id="msection">
+                        <option value="selected hidden">Select Your Section</option>
+                        <option value="HR">HR</option>
+                        <option value="Admin">Admin</option>
+                        <option value="Other">Other</option>
+</select>
+                </div>
+            </div>
+            <div class="content">
+                <div class="input-box">
+                    <label for="name">Birth Day</label>
+                    <input type="date" id="mbdate">
+                </div>
+            </div>
+            <div class="content">
+                <lable>Conform</lable>
+                <input type="radio" name="conform" value="0" id="mcfirm">NO
+                <input type="radio" name="conform" value="1" id="mcfirm">YES
 
             </div>
-        </div>
-    </form>
-    <div id="mdata">............................</div>
-    <?php
+            <div class="content">
+                <div class="button">
+                    <input type="submit" onclick="find()">
+                </div>
+            </div>
+        </form>
+        <div id="mdata">............................</div>
+        <?php
     include "db.php";
     $str1="SELECT * FROM member order by mid";
     $rs1=$bdd-> query($str1) or die ("erorr on $str1");
     ?>
 
-    <!--Table-->
-    <table>
-        <thead>
-            <tr>
-                <th>Member ID</th>
-                <th>Company ID</th>
-                <th>Member Name</th>
-                <th>Address</th>
-                <th>Tellephon num</th>
-                <th>Register Date</th>
-                <th>NIC</th>
-                <th>Section</th>
-                <th>Birth Day</th>
-                <th>Conformatin</th>
-                <th></th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while($row1=$rs1->fetch()){ ?>
-            <tr>
-                <td>
-                    <?php echo $row1[0]?>
-                </td>
-                <td>
-                    <?php echo $row1[1]?>
-                </td>
-                <td>
-                    <?php echo $row1[2]?>
-                </td>
-                <td>
-                    <?php echo $row1[3]?>
-                </td>
-                <td>
-                    <?php echo $row1[4]?>
-                </td>
-                <td>
-                    <?php echo $row1[5]?>
-                </td>
-                <td>
-                    <?php echo $row1[6]?>
-                </td>
-                <td>
-                    <?php echo $row1[7]?>
-                </td>
-                <td>
-                    <?php echo $row1[8]?>
-                </td>
-                <td>
-                    <?php echo $row1[9]?>
-                </td>
-                <td><input type="button" value="Edit" class="form-control text-bg-warning" id="<?php echo $row1[0]?>"
-                        onclick="findbk(this.id)"></td>
-                <td>
-                    <input type="button" value="Delete" class="form-control text-bg-danger" id="<?php echo $row1[0] ?>"
-                        onclick="deletebk(this.id)">
-                </td>
-            </tr>
-            <?php }?>
-        </tbody>
-    </table>
+        <!--Table-->
+        <table>
+            <thead>
+                <tr>
+                    <th>Member ID</th>
+                    <th>Company ID</th>
+                    <th>Member Name</th>
+                    <th>Address</th>
+                    <th>Tellephon num</th>
+                    <th>Register Date</th>
+                    <th>NIC</th>
+                    <th>Section</th>
+                    <th>Birth Day</th>
+                    <th>Conformatin</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while($row1=$rs1->fetch()){ ?>
+                <tr>
+                    <td>
+                        <?php echo $row1[0]?>
+                    </td>
+                    <td>
+                        <?php echo $row1[1]?>
+                    </td>
+                    <td>
+                        <?php echo $row1[2]?>
+                    </td>
+                    <td>
+                        <?php echo $row1[3]?>
+                    </td>
+                    <td>
+                        <?php echo $row1[4]?>
+                    </td>
+                    <td>
+                        <?php echo $row1[5]?>
+                    </td>
+                    <td>
+                        <?php echo $row1[6]?>
+                    </td>
+                    <td>
+                        <?php echo $row1[7]?>
+                    </td>
+                    <td>
+                        <?php echo $row1[8]?>
+                    </td>
+                    <td>
+                        <?php echo $row1[9]?>
+                    </td>
+                    <td><input type="button" value="Edit" class="form-control text-bg-warning"
+                            id="<?php echo $row1[0]?>" onclick="findbk(this.id)"></td>
+                    <td>
+                        <input type="button" value="Delete" class="form-control text-bg-danger"
+                            id="<?php echo $row1[0] ?>" onclick="deletebk(this.id)">
+                    </td>
+                </tr>
+                <?php }?>
+            </tbody>
+        </table>
 
 </body>
 <script type='text/javascript' src='jquery.min.js'></script>
@@ -274,11 +213,11 @@
 
         });
     }
-    function findbk(bno) {
+    function findbk(mid) {
         alert('ok');
         $.ajax({
             type: "post",
-            data: { pvals: bno },
+            data: { pvals: mid },
             url: "findm.php",
             success: function (json) { $("#mdata").html(json) }
         });
