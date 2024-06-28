6bbb11fefe95e919df7715048f2984024970724b
dandaboy0 <ravinduanupama30@gmail.com>
2024-06-28 11:18:30 +0530

    up

--- index.php
+++ index.php
@@ -133,7 +133,7 @@
                 </div>
             </div>
         </form>
-        <div id="mdata">..............hjhjhjhhh..............</div>
+        <div id="mdata">............................</div>
         <?php
     include "db.php";
     $str1="SELECT * FROM member order by mid";
