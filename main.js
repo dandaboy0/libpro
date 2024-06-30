7414b1df91b134f5a5b83b7202dd0b92cfde2ed7
dandaboy0 <ravinduanupama30@gmail.com>
2024-06-30 22:29:44 +0530

    delete

--- main.js
+++
@@ -1,8 +0,0 @@
-function find() {
-    var vals=$("input").map(function(){return $(this).val();}).get()
-    $.ajax({type:"post",
-    data:{   },
-    url:"savest.php",
-    success:function(json){alert(json)}
-    });
-}
